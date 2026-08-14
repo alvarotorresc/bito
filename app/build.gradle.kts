cd0ee304dfb1714e8d1e2bf7c31e194d89bd0a3b
Alvaro Torres <alvarotorresc@users.noreply.github.com>
2026-08-14 22:45:50 +0200

    chore(build): add junit and kotlin-test unit test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,6 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlin.test.junit)
 }
